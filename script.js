6207aac6868f1f9909a629e4f815facd6f4a0cb2
Corby7 <corbijn.bulsink@hotmail.com>
2025-01-15 17:02:35 +0100

    main script

---
+++ script.js
@@ -0,0 +1,21 @@
+import Tree from "./tree.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const test = new Tree();
+test.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+prettyPrint(test.root);
+test.deleteItem(67);
+prettyPrint(test.root);
